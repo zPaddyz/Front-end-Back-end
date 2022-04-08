6ad271fc093dcf7ed5e4575b26ac41a10b072102
Fadl Matar <s195846@student.dtu.dk>
2022-04-08 12:08:59 +0200

    Routing done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Login from "./Login";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import Register from "./Register";
+import Home from "./Home";
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -19,18 +23,31 @@ function App() {
   }, []);
 
   return (
+  <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Dette er skrevet i frontend</p>
-        
-        
-        <p>{!data ? "Loading..." : data}</p>
+      {/*<header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+           <p>Dette er skrevet i frontend</p>
 
-        <p>{!data1 ? "Loading..." : data1}</p>
 
-      </header>
+           <p>{!data ? "Loading..." : data}</p>
+
+            <p>{!data1 ? "Loading..." : data1}</p>
+
+          </header>*/}
+      <Switch>
+        <Route exact path="/">
+          <Login/>
+        </Route>
+        <Route exact path="/register">
+          <Register/>
+        </Route>
+        <Route exact path="/home">
+          <Home/>
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,5 +1,6 @@
-import {AppBar, Button, Link, makeStyles, Toolbar, Typography} from "@mui/material";
-import Plannerino from "./img/Plannerino.png";
+import {AppBar, Button, makeStyles, Toolbar, Typography} from "@mui/material";
+import Plannerino from "./Plannerino.png";
+import { Link } from 'react-router-dom';
 
 
 const Home = () =>{
@@ -9,7 +10,9 @@ const Home = () =>{
                 <Toolbar>
                     <img src={Plannerino} alt={"bibo"} height={"40px"} width={"350px"} style={{padding: "20px 0px", marginTop: ""}}/>
                     <div style={{backgroundColor: "#D5A13E", marginLeft: "1000px", padding: "", borderRadius: "5px"}}>
-                        <Button style={{color: "white"}}>login</Button>
+                        <Link to="/" style={{textDecoration: "none"}}>
+                            <Button style={{color: "white"}}>log out</Button>
+                        </Link>
                     </div>
                 </Toolbar>
             </AppBar>
