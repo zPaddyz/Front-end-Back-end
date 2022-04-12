dbad5bf84fc7205b0f565d667194254310605686
zPaddyz <jul2009@live.dk>
2022-04-12 18:23:00 +0200

    arbejde på eventbox

---
+++ client/src/EventBox.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./EventBox.css";
+import Valley from "./Valley.jpg"
+import city from "./city.jpg"
+import { useState, useEffect, useRef } from "react";
+
+function EventBox(props){
+    const obj = useRef(0);
+
+    const fetchData = async () => {
+      const data = await fetch(props.endpoint)
+    .then(res => res.json())
+    .then(haha => obj.current = haha)
+    .then(console.log(obj.current))
+    .then(() => !localStorage.getItem(props.get) ? localStorage.setItem(props.get, JSON.stringify(obj.current)) : console.log("already exists"))
+  }
+
+  return fetchData(),displayItem(props.get)
+}   
+
+function displayItem(get){
+    const title = JSON.parse(localStorage.getItem(get)).title;
+    const date = JSON.parse(localStorage.getItem(get)).date;
+    const picture = JSON.parse(localStorage.getItem(get)).picture;
+    const color = JSON.parse(localStorage.getItem(get)).title;
+        return(
+            <div className="eventBoxes">
+                <button className="myButton" style={{backgroundColor: color}}>
+                <p className ="title">{title}</p>
+                <img id="test"/>
+                {
+                (picture === "Valley")? <img src={Valley} className="centerImage"/> : 
+                (picture === "city")? <img src={city} className="centerImage"/>:
+                <p className="centerImage2">*Intet billede*</p>
+                }
+
+                <p className = "date">{date}</p>
+                </button>
+            </div>
+        )
+}
+
+function picturePick(props){
+    document.getElementById("imageid").src="../template/save.png";
+    const Tpicture = props.picture;
+    if(Tpicture === "Valley") document.getElementById("test").src="./Valley.jpg";
+    else if(Tpicture === "city")document.getElementById("test").src="./city.jpg";
+    else return(<p className="centerImage">*Intet billede*</p>)
+}
+export default EventBox;
--- server/index.js
+++ server/index.js
@@ -11,7 +11,26 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/apis", (req, res) => {
-    res.json({ message: "Hello from server!"});
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.json({ title: "Bryllupsdag",picture : "city", date : "14/4/2022"});
+  });
+  app.get("/apitest", (req, res) => {
+    res.set('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.json({ title: "Test",picture : "Valley", date : "22/7/2029"});
+  });
+
+  app.get("/test", (req, res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    res.json([{ firstName: "Tester",lastName : "Valley"}]);
+  });
+
+  app.post("/test", (req,res) => {
+    res.set('Access-Control-Allow-Origin', '*');
+    const user ={
+      firstName: req.body.firstName,
+      lastName: req.body.lastName
+    }
+    res.json( user);
   });
 
 app.listen(PORT, () => {
