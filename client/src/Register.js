2671082e4ccdcd1ade75f0ec5eb34e60d189aa13
zPaddyz <jul2009@live.dk>
2022-04-15 01:25:44 +0200

    tilføjet brugeroprettelse i database (ikke færdig)

--- client/src/Register.js
+++ client/src/Register.js
@@ -3,6 +3,7 @@ import Plannerino from "./Plannerino.png"
 import {Button, Grid, TextField} from "@mui/material";
 import {makeStyles} from "@mui/styles";
 import { Link } from 'react-router-dom';
+import React from "react";
 
 const useStyles = makeStyles({
     paperStyle: {
@@ -30,35 +31,61 @@ const useStyles = makeStyles({
     }
 });
 
+function saveData(Firstname,LastName,Email,Password) {
+    // POST request using fetch with set headers
+    const requestOptions = {
+        method: 'POST',
+        headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer my-token',
+            'My-Custom-Header': 'foobar'
+        },
+        body: JSON.stringify({ 
+        firstName: Firstname,
+        lastName: LastName,
+        email: Email,
+        password: Password})
+    };
+    fetch('/user', requestOptions)
+        .then(response => response.json())
+        .then(data => this.setState({ postId: data.id }));
+
+        
+}
 
 const Register = () => {
+    var firstNameRef = React.useRef('');
+    var lastNameRef = React.useRef('');
+    var emailRef = React.useRef('');
+    var passwordRef = React.useRef('');
+
     const classes = useStyles();
     return(
+        
         <div>
             <Paper elevation={24} className={classes.paperStyle}>
                 <img src={Plannerino} alt={"bibo"} className={classes.imageStyle}/>
                 <Grid>
                     <Grid item xs={12}>
-                        <TextField label="Username" variant="standard"/>
+                        <TextField inputRef={ref => { firstNameRef = ref; }} label="Username" variant="standard"/>
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField label="E-mail" variant="standard"/>
+                        <TextField inputRef={ref => { emailRef = ref; }}label="E-mail" variant="standard"/>
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField label="Password" variant="standard" type="password" style={{marginTop: "10px"}}/>
+                        <TextField inputRef={ref => { passwordRef = ref; }} label="Password" variant="standard" type="password" style={{marginTop: "10px"}}/>
                     </Grid>
                     <Grid item xs={12}>
-                        <TextField label="Confirm password" variant="standard" type="password" style={{marginTop: "10px"}}/>
+                        <TextField /*inputRef={ref => { passwordRef = ref; }}*/ label="Confirm password" variant="standard" type="password" style={{marginTop: "10px"}}/>
                     </Grid>
                     <Grid item xs={12}>
-                        <Link to="/home" style={{textDecoration: "none"}}>
-                            <Button variant="contained" className={classes.buttonStyle}>Get planning</Button>
-                        </Link>
+                        
+                            <Button onClick={ () => saveData(firstNameRef.value, firstNameRef.value, emailRef.value,passwordRef.value)} variant="contained" className={classes.buttonStyle}>Get planning</Button>
+                        
                     </Grid>
                 </Grid>
             </Paper>
         </div>
     )
 }
-
 export default Register;
